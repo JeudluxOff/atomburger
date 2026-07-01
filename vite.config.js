b2c41ee997a29c11d0203bf0bfc79811ac7e8ace
JeudluxOff <jeudluxoff@gmail.com>
2026-07-01 15:24:40 +0200

    V.5.6

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: ['VITE_', 'STORAGE_', 'NEXT_PUBLIC_STORAGE_'],
-})
+node_modules
+dist
+.env
+.vercel
